d5b02e20544547232d875900a36c773d394a5b1a
David Nakov <nakovdavid98@gmail.com>
2024-12-23 12:32:53 +0200

    feat: added a section for subscribing to a newsletter;

--- app/page.tsx
+++ app/page.tsx
@@ -65,58 +65,87 @@ const HomePage = () => {
         community and let&apos;s explore the world of development, one post at a
         time.
       </section>
+      <div className="mt-6" />
+      <section className="bg-indigo-400 p-8 rounded-xl">
+        <h2 className="text-lg text-center">
+          Stay Updated Don&apos;t miss out!
+        </h2>
+        <div className="mt-2" />
+        <p className="max-w-md mx-auto text-center">
+          Subscribe to our newsletter to receive the latest updates straight to
+          your inbox.
+        </p>
+        <div className="mt-3" />
+        <div className="flex gap-x-6 justify-center">
+          <label className="flex flex-col w-full max-w-lg">
+            <span className="block">Your email address</span>
+            <div className="mt-1" />
+            <input
+              type="email"
+              className="p-1 bg-pink-100 rounded text-gray-600 w-full"
+              required
+              min={8}
+            />
+          </label>
+          <button className="self-end inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+            Subscribe!
+          </button>
+        </div>
+      </section>
+      <div className="mt-6" />
       <section>
         <h2>Explore some of our best articles</h2>
         <div className="mt-3" />
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {blogPosts.map((post, index) => (
-          <div
-            key={index}
-            className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-          >
-            <Link href="#">
-              <img
-                className="rounded-t-lg"
-                src="/docs/images/blog/image-1.jpg"
-                alt=""
-              />
-            </Link>
-            <div className="p-5">
-              <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                  Noteworthy technology acquisitions 2021
-                </h5>
-              </a>
-              <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                Here are the biggest enterprise technology acquisitions of 2021
-                so far, in reverse chronological order.
-              </p>
-              <a
-                href="#"
-                className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-              >
-                Read more
-                <svg
-                  className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
+          {blogPosts.map((post, index) => (
+            <div
+              key={index}
+              className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+            >
+              <Link href="#">
+                <img
+                  className="rounded-t-lg"
+                  src="/docs/images/blog/image-1.jpg"
+                  alt=""
+                />
+              </Link>
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    Noteworthy technology acquisitions 2021
+                  </h5>
+                </a>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order.
+                </p>
+                <a
+                  href="#"
+                  className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </a>
+                  Read more
+                  <svg
+                    className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 14 10"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M1 5h12m0 0L9 1m4 4L9 9"
+                    />
+                  </svg>
+                </a>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
         </div>
       </section>
+      <div className="mt-6" />
     </div>
   );
 };
