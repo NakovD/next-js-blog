af5b02ce7199241497a5efe5ef86cfbab035881e
David Nakov <nakovdavid98@gmail.com>
2025-01-28 19:53:55 +0200

    feat: display post creation date on post details page

--- app/post-details/[id]/page.tsx
+++ app/post-details/[id]/page.tsx
@@ -11,8 +11,8 @@ const BlogPage = async ({ params }: { params: Promise<{ id: string }> }) => {
     <>
       <div className="text-center pt-16 md:pt-32">
         <p className="text-sm md:text-base text-green-500 font-bold">
-          04 AUGUST 2023 <span className="text-gray-900">/</span> GETTING
-          STARTED
+          {post.createdAt.toDateString()}{" "}
+          <span className="text-gray-900">/</span> GETTING STARTED
         </p>
         <h1 className="font-bold break-normal text-3xl md:text-5xl">
           {post.name}
