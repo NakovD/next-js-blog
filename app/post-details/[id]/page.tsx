51b87fc52a0ff0d0117194f1511d41de2296cc8e
David Nakov <nakovdavid98@gmail.com>
2025-01-31 18:58:24 +0200

    refactor: update post details page to display last updated date instead of creation date

--- app/post-details/[id]/page.tsx
+++ app/post-details/[id]/page.tsx
@@ -11,7 +11,7 @@ const BlogPage = async ({ params }: { params: Promise<{ id: string }> }) => {
     <>
       <div className="text-center pt-16 md:pt-32">
         <p className="text-sm md:text-base text-green-500 font-bold">
-          {post.createdAt.toDateString()}{" "}
+          {post.updatedAt.toDateString()}{" "}
           <span className="text-gray-900">/</span> GETTING STARTED
         </p>
         <h1 className="font-bold break-normal text-3xl md:text-5xl">
