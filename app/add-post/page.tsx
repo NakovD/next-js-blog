c39770bdbb301f4426a8612719bd5b6a4b9741cd
David Nakov <nakovdavid98@gmail.com>
2025-03-16 09:11:14 +0200

    feat: implement form input for post name in AddPostPage

--- app/add-post/page.tsx
+++ app/add-post/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { Controller } from "react-hook-form";
+import { Input } from "@/app/components/ui/form/Input";
+import { useAppForm } from "@/app/hooks/useAppForm";
+import { Label } from "@/app/components/ui/form/Label";
+
 const AddPostPage = () => {
+  const { control } = useAppForm<{ name: string }>();
+
   return (
     <div>
-      <h1>Hi, I am Dawda</h1>
+      <Controller
+        control={control}
+        name="name"
+        render={({ field: { name, ...rest } }) => (
+          <Label htmlFor={name}>
+            Name
+            <Input id={name} {...rest} />
+          </Label>
+        )}
+      />
     </div>
   );
 };
