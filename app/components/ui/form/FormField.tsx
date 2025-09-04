7a0fb6cbf99249ef6306c77fde6e9edc98e7ad07
David Nakov <nakovdavid98@gmail.com>
2025-09-04 19:49:57 +0300

    refactor: enhance FormField component to support generic field values and names

--- app/components/ui/form/FormField.tsx
+++ app/components/ui/form/FormField.tsx
@@ -1,8 +1,16 @@
 "use client";
 
 import { ComponentProps } from "react";
-import { Controller } from "react-hook-form";
+import { Controller, FieldPath, FieldValues } from "react-hook-form";
 
-type FormFieldProps = ComponentProps<typeof Controller>;
+type FormFieldProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> = ComponentProps<typeof Controller<TFieldValues, TName>>;
 
-export const FormField = (props: FormFieldProps) => <Controller {...props} />;
+export const FormField = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>(
+  props: FormFieldProps<TFieldValues, TName>
+) => <Controller {...props} />;
